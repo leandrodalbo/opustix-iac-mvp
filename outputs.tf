c4b9dfe3b4992c5adf8a50653269a377b627554a
leandrodalbo <leandro.edbo@gmail.com>
2025-07-07 17:36:25 -0300

    extra ec2/private s3 changes

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "ec2_public_ip" {
   value = aws_eip.mvp_eip.public_ip
 }
 
+output "ec2_auth_public_ip" {
+  value = aws_eip.auth_eip.public_ip
+}
+
 output "banners_bucket_name" {
   value = aws_s3_bucket.banners.bucket
 }
--- s3.tf
+++ s3.tf
@@ -2,8 +2,18 @@ resource "aws_s3_bucket" "banners" {
   bucket        = "mvp-banners-bucket-${var.env}"
   force_destroy = true
 
+  
   tags = {
     Name        = "Banners Bucket"
     Environment = var.env
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "banners_block" {
+  bucket = aws_s3_bucket.banners.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
